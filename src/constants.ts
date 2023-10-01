99171d7f5ded4c0d0ad2395da631eba5ec9280d4
Antonio Alvarez <antonioalvarez713@gmail.com>
2023-10-01 19:22:28 -0400

    api is missing

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
 import dotenv from 'dotenv';
 dotenv.config()
 
-export const PARITY_SERVER = process.env.PARITY_SERVER || 'http://ec2-34-203-229-133.compute-1.amazonaws.com:3000'
+export const PARITY_SERVER = process.env.PARITY_SERVER || 'http://ec2-34-203-229-133.compute-1.amazonaws.com:3000/api'
 export const JWT_SECRET = process.env.JWT_SECRET || 'tiJzeJyGtF78vhdgI45pelbdd8zzIXH1lA1R96xtCaptrsFU6Xtnezon2lFGVl9b'
